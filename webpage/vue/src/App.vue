27a0a0df770ec677feb4584b628a2829d0f850dc
KobyMcGuire <mcguirej2019@gmail.com>
2024-01-03 13:54:53 -0500

    Router view to tricks set up

--- webpage/vue/src/App.vue
+++ webpage/vue/src/App.vue
@@ -1,40 +1,56 @@
 <template>
   <v-app>
+    <v-navigation-drawer expand-on-hover rail>
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          title="Koby McGuire"
+          subtitle="testEmail@gmail.com"
+        ></v-list-item>
+      </v-list>
 
-    <v-navigation-drawer
-        expand-on-hover
-        rail
-      >
-        <v-list>
-          <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            title="Sandra Adams"
-            subtitle="sandra_a88@gmailcom"
-          ></v-list-item>
-        </v-list>
+      <v-divider></v-divider>
 
-        <v-divider></v-divider>
+      <v-list density="compact" nav>
+        <v-list-item
+          prepend-icon="mdi-skateboard"
+          title="My Bag of Tricks"
+          value="myTricks"
+          to="/tricks"
+          link
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account-multiple"
+          title="Shared with me"
+          value="shared"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-star"
+          title="Starred"
+          value="starred"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    
+    <v-app-bar title="Trick Bag"></v-app-bar>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-
-
-export default {  
-
-
-};
+export default {};
 </script>
 
 <style>
+* {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
 
 .error-message {
   background-color: #fca5a5;
