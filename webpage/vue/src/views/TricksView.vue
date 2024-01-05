43b7a7930fbe77189f068a11d1fce046cac2c57d
KobyMcGuire <mcguirej2019@gmail.com>
2024-01-05 00:02:19 -0500

    Form Stlying

--- webpage/vue/src/views/TricksView.vue
+++ webpage/vue/src/views/TricksView.vue
@@ -1,9 +1,8 @@
 <template>
   <v-sheet class="d-flex flex-column ga-10">
-  
     <v-sheet class="d-flex ga-10">
       <v-sheet rounded class="pa-3 elevation-10 bg-grey-lighten-3">
-        <h2 class="tricks-header">Known Tricks</h2>
+        <h2 class="tricks-header">Your Bag</h2>
         <div class="trick-cards-container">
           <trick-card
             v-for="trick in knownTricks"
@@ -15,7 +14,7 @@
       </v-sheet>
 
       <v-sheet rounded class="pa-3 elevation-10 bg-grey-lighten-3">
-        <h2 class="tricks-header">Unknown Tricks</h2>
+        <h2 class="tricks-header">In-Progress Tricks</h2>
         <div class="trick-cards-container">
           <trick-card
             v-for="trick in unknownTricks"
@@ -27,47 +26,66 @@
       </v-sheet>
     </v-sheet>
 
+    <v-expand-transition>
+      <v-form v-if="showNewTrickForm" @submit.prevent>
+        <v-text-field
+          v-model="newTrick.name"
+          label="Trick Name"
+          :rules="trickNameRules"
+        ></v-text-field>
 
+        <v-text-field
+          v-model="newTrick.stance"
+          label="Stance"
+          :rules="stanceNameRules"
+        ></v-text-field>
 
+        <p>Is this trick in your bag?</p>
+        <v-radio-group v-model="inBagRadio" :rules="inBagRules" inline>
+          <v-radio label="Yes" value="Yes"></v-radio>
+          <v-radio label="No" value="No"></v-radio>
+        </v-radio-group>
 
+        <p>Is this trick composed of a Shuv, Flip, or both?</p>
+        <v-radio-group
+          v-model="flipShuvRadio"
+          :rules="flipShuvRadioRules"
+          inline
+        >
+          <v-radio label="Shuv" value="Shuv"></v-radio>
+          <v-radio label="Flip" value="Flip"></v-radio>
+          <v-radio label="Combination" value="Flip-Shuv"></v-radio>
+        </v-radio-group>
 
-    <v-form v-if="showNewTrickForm" @submit.prevent fast-fail>
-      <div class="new-trick-error-message error-message" v-if="newTrickError">
-        <p>You must complete all fields</p>
-      </div>
+        <div class="d-flex justify-center ga-3">
+          <v-btn
+            type="submit"
+            class="bg-green-lighten-1"
+            width="300"
+            text="Submit"
+            @click="
+              (expandNewTrickForm = !expandNewTrickForm), handleSubmitNewTrick
+            "
+          ></v-btn>
 
-      <v-text-field
-        v-model="newTrick.name"
-        label="Trick Name"
-        :rules="trickNameRules"
-      ></v-text-field>
+          <v-btn
+            class="bg-red-lighten-1"
+            width="300"
+            text="Cancel"
+            @click="showNewTrickForm = !showNewTrickForm"
+          ></v-btn>
+        </div>
+      </v-form>
+    </v-expand-transition>
 
-      <v-text-field
-        v-model="newTrick.stance"
-        label="Stance"
-        :rules="stanceNameRules"
-      ></v-text-field>
-
-      <p>Is this trick in your bag?</p>
-      <v-radio-group v-model="inBagRadio" :rules="inBagRules" inline>
-        <v-radio label="Yes" value="Yes"></v-radio>
-        <v-radio label="No" value="No"></v-radio>
-      </v-radio-group>
-
-      <p>Is this trick composed of a Shuv, Flip, or both?</p>
-      <v-radio-group v-model="flipShuvRadio" :rules="flipShuvRadioRules" inline>
-        <v-radio label="Shuv" value="Shuv"></v-radio>
-        <v-radio label="Flip" value="Flip"></v-radio>
-        <v-radio label="Combination" value="Flip-Shuv"></v-radio>
-      </v-radio-group>
-
-      <input type="submit" @click="handleSubmitNewTrick" />
-    </v-form>
-
-    <v-btn v-on:click="handleCreateTrick" prepend-icon="mdi-plus" class="bg-blue-grey-lighten-4">
+    <v-btn
+      v-on:click="handleCreateTrick"
+      prepend-icon="mdi-plus"
+      class="bg-blue-grey-lighten-4 mx-auto"
+      width="300"
+    >
       Create a New Trick
     </v-btn>
-
   </v-sheet>
 </template>
 
@@ -83,41 +101,41 @@ export default {
       tricks: [],
       knownTricks: [],
       unknownTricks: [],
+      expandNewTrickForm: false,
       newTrick: {},
-      newTrickName : "",
-      trickNameRules : [
-        value => {
-          if (value?.length > 0) return true
+      newTrickName: "",
+      trickNameRules: [
+        (value) => {
+          if (value?.length > 0) return true;
 
-          return 'Trick name must not be left empty'
-        }
+          return "Trick name must not be left empty";
+        },
       ],
-      newTrickStance : "",
-      stanceNameRules : [
-        value => {
-          if (value?.length > 0) return true
+      newTrickStance: "",
+      stanceNameRules: [
+        (value) => {
+          if (value?.length > 0) return true;
 
-          return 'Stance must not be left empty'
-        }
+          return "Stance must not be left empty";
+        },
       ],
-      inBagRadio : "",
-      inBagRules : [
-        value => {
-          if (value?.length > 0) return true
+      inBagRadio: "",
+      inBagRules: [
+        (value) => {
+          if (value?.length > 0) return true;
 
-          return 'You must select an option'
-        }
+          return "You must select an option";
+        },
       ],
-      flipShuvRadio : "",
-      flipShuvRadioRules : [
-        value => {
-          if (value?.length > 0) return true
+      flipShuvRadio: "",
+      flipShuvRadioRules: [
+        (value) => {
+          if (value?.length > 0) return true;
 
-          return 'You must select an option'
-        }
+          return "You must select an option";
+        },
       ],
       showNewTrickForm: false,
-      newTrickError: false,
     };
   },
 
@@ -153,21 +171,16 @@ export default {
     },
 
     handleSubmitNewTrick() {
-      this.checkFormValid();
-
       if (!this.newTrickError) {
         // Map the isKnown and flipOrShuv
-        this.newTrick.known = document.querySelector(
-          "input[name=is-known]:checked"
-        ).value;
-        this.newTrick.flipOrShuv = document.querySelector(
-          "input[name=flip-or-shuv]:checked"
-        ).value;
+        this.newTrick.known = this.inBagRadio;
+        this.newTrick.flipOrShuv = this.flipShuvRadio;
 
         TrickService.createTrick(this.newTrick)
           .then((response) => {
-            // Reset New Trick Object
+            // Reset New Trick Object and Close New Trick Form
             this.newTrick = {};
+            this.showNewTrickForm = !this.showNewTrickForm;
           })
           .catch((error) => {
             this.errorHandler(error, "creating");
