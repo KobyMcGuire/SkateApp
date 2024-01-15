7d4998733a83457e5ed7528407c4ccd1001c7e5f
KobyMcGuire <mcguirej2019@gmail.com>
2024-01-15 17:31:40 -0500

    Search half way working

--- webpage/vue/src/components/TrickSearchBar.vue
+++ webpage/vue/src/components/TrickSearchBar.vue
@@ -2,11 +2,15 @@
     <v-toolbar
       dense
       floating
+      height="50"
+      rounded
     >
       <v-text-field
         hide-details
         prepend-icon="mdi-magnify"
         single-line
+        v-model="searchTerm"
+        v-on:keyup="filterTricks()"
       ></v-text-field>
 
     </v-toolbar>
@@ -16,6 +20,29 @@
 export default {
     props: ["isKnown"],
 
+    data() {
+      return {
+        searchTerm : ""
+      }
+    },
+
+    methods :  {
+      filterTricks() {
+        let tricks = ""
+        if (this.isKnown === 'Yes') {
+          this.$store.state.inBagTricks = this.$store.state.inBagTricks.filter((trick) => {
+            if (trick.name.includes(this.searchTerm)) {
+              return true;
+            }
+          })
+        }
+        else {
+          tricks = "no";
+        }
+        console.log(this.searchTerm);
+        return tricks;
+      },
+    }
 }
 </script>
 
