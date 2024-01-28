d1fc7b05abecebb66d47c34f11ea218a63f822f4
KobyMcGuire <mcguirej2019@gmail.com>
2024-01-28 15:15:25 -0500

    Work on landing page

--- webpage/vue/src/components/TheWelcome.vue
+++ webpage/vue/src/components/TheWelcome.vue
@@ -1,15 +1,23 @@
 <template>
-  <div id="welcome">
-    <h1>Welcome to your bag of tricks</h1>
-  </div>
+  <v-sheet class="d-flex justify-center align-center ga-5">
+
+    <v-sheet width="45%">
+      <h1>Welcome to your Bag</h1>
+      <p class="text-center">
+        A tool that will enhance and bolster your growth in skateboarding.
+        Record what tricks you know, and what tricks you want to learn, and
+        allow our tool to recommend you these tricks in lines, or to take with
+        you to your next practice sesh.
+      </p>
+    </v-sheet>
+
+    <img src="../assets/grabPic.PNG" alt="Man grabbing skateboard mid-air" />
+
+  </v-sheet>
 </template>
 
 <script>
-import { assert } from '@vue/compiler-core';
-
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
@@ -21,42 +29,13 @@ export default {
 }
 
 h1 {
-  width: 500px;
-
   font-size: 5rem;
-
   text-align: center;
-  }
-
-  img {
-    border-radius: 3px; 
-
-    position: relative;
-  }
-
-.tre {
-  width: 800px;
-  height: auto;
-
-  z-index: 1;
 }
 
-.grab {
-  height: 500px;
-  width: auto;
+img {
+  border-radius: 3px;
 
-  z-index: 2;
-  bottom: 5%;
-  left: -10%;
-
-}
-.smith {
-  height: 500px;
-  width: auto;
-
-  z-index: 3;
-  left: 25%;
-  bottom: 10%;
-
+  position: relative;
 }
 </style>
