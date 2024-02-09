9e75b48a80c025e1e23d19da1a72d1818f4b0520
KobyMcGuire <mcguirej2019@gmail.com>
2024-02-09 14:05:07 -0500

    Work on generating practice bag

--- webpage/vue/src/views/PracticeBagView.vue
+++ webpage/vue/src/views/PracticeBagView.vue
@@ -3,11 +3,16 @@
         <h1>Generate a Practice Bag for your next Session</h1>
 
         <!-- TODO: FILL OUT RULES??? -->
+        <v-form @submit.prevent ref="form">
+            <v-select v-model="newBagMix" :items="mixChoices" :rules="practiceBagMixRules"
+                label="Practice Bag Composition"></v-select>
 
-        <v-select v-model="newBagMix" :items="mixChoices" :rules="stanceNameRules" label="Practice Bag Composition"></v-select>
+            <v-slider min="1" :max="newBagMaxLength" label="How many tricks would you like in this bag?" :step="1" thumb-label="always"></v-slider>
 
-        <v-slider min="1" :max="newBagMaxLength" label="How many tricks would you like in this bag?"  :step="1"
-            thumb-label="true"></v-slider>
+            <v-sheet class="d-flex justify-center">
+                <v-btn type="submit" class="bg-green-lighten-1" width="300" text="Create" @click="validateForm()"></v-btn>
+            </v-sheet>
+        </v-form>
 
         <v-divider class="my-10"></v-divider>
 
@@ -18,7 +23,7 @@
                 :practiceBag="practiceBag"></practice-bag-card>
         </v-sheet>
 
-        
+
     </v-sheet>
 </template>
 
@@ -39,7 +44,15 @@ export default {
                 "In-Progress Tricks",
                 "A mix of In-Bag and In-Progress Tricks"
             ],
-            newBagMix: ""
+            newBagMix: "",
+
+            practiceBagMixRules: [
+                (value) => {
+                    if (value?.length > 0) return true;
+
+                    return "Stance must not be left empty";
+                },
+            ],
         }
     },
 
@@ -57,14 +70,14 @@ export default {
 
         // Set maximum length of new Practice bag (Grab all tricks from DB and take length)
         TrickService.getKnownTricks()
-        .then((response) => {
-            this.$store.state.knownTricks = response.data;
-        })
-        
+            .then((response) => {
+                this.$store.state.knownTricks = response.data;
+            })
+
         TrickService.getUnknownTricks()
-        .then((response) => {
-            this.$store.state.unknownTricks = response.data;
-        })
+            .then((response) => {
+                this.$store.state.unknownTricks = response.data;
+            })
 
     },
 
@@ -99,11 +112,27 @@ export default {
             console.log(`There was an error ${verb}, the error was: ${error}`);
         },
 
-
         // Store methods
         addPraticeBagsToStore(practiceBags) {
             this.$store.state.practiceBags = practiceBags;
-        }
+        },
+
+        generatePracticeBag() {
+            console.log("Here");
+        },
+
+
+        // Form Validation Method
+        async validateForm() {
+            const { valid } = await this.$refs.form.validate()
+
+            if (valid) {
+                this.generatePracticeBag();
+            }
+            else {
+                alert("Form is invalid");
+            }
+        },
     }
 
 }
